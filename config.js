7d1979ec45da3e13bba599a789744c2624cd69ab
Zeb Pirzada <50837526+zpirzada@users.noreply.github.com>
2026-01-15 00:09:53 +0100

    Files uploaded

---
+++ config.js
@@ -0,0 +1,12 @@
+// Config file created for Karlebo Club Calendar
+// - sheetCsvUrl: Publish to web -> CSV link
+// created by Zeb Pirzada - Jan 2026
+const CONFIG = {
+  sheetCsvUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRihK3ajfi2Q9iuBBaR-I36VlYVWpndGYUlmfupg2-dqrn6pMv3vrKeIIWcp2UEQTbeoKdMsV8zU4Q6/pub?gid=0&single=true&output=csv",
+  dateFormat: "DMY",              // DMY (Denmark) or MDY
+  timeFallbackMinutes: 90,        // if end time missing, add minutes to start
+  colorByGroup: {
+    "Karlebo Club": "#e71b1bff",
+    "FCC Board": "#661be7ff"
+  }
+};
